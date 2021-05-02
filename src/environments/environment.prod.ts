1247b0866a14a745e7f1e47fcb2ba010b3da3f13
rcebrian <rodrigocebriang@gmail.com>
2021-05-03 00:30:29 +0200

    :heavy_plus_sign: ng2-completer

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,6 +3,14 @@
  * Copyright Akveo. All Rights Reserved.
  * Licensed under the MIT License. See License.txt in the project root for license information.
  */
+const authBase = `http://localhost:3000/api/auth`;
+
 export const environment = {
   production: true,
+
+  authBase: authBase,
+
+  auth: {
+    signIn: `/signIn`,
+  }
 };
