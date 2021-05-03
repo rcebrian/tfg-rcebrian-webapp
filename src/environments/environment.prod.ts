9778625357076632bf14f7540ba36c610c6b90af
rcebrian <rodrigocebriang@gmail.com>
2021-05-03 19:57:09 +0200

    prod env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,13 +4,18 @@
  * Licensed under the MIT License. See License.txt in the project root for license information.
  */
 const authBase = `http://localhost:3000/api/auth`;
+const usersBase = `http://localhost:3001/api/users`;
 
 export const environment = {
   production: true,
 
   authBase: authBase,
+  usersBase: usersBase,
 
   auth: {
     signIn: `/signIn`,
+  },
+  users: {
+    getUserInfo: `${usersBase}/:id`
   }
 };
