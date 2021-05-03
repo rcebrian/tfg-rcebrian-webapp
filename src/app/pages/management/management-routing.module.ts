62fd3a240d1664efa093aa2d66b4fa297524c7c7
rcebrian <rodrigocebriang@gmail.com>
2021-05-03 23:17:47 +0200

    only ROLE_ADMIN

--- src/app/pages/management/management-routing.module.ts
+++ src/app/pages/management/management-routing.module.ts
@@ -4,10 +4,13 @@ import { Routes, RouterModule } from '@angular/router';
 import { ManagementComponent } from './management.component';
 import { IconsComponent } from './icons/icons.component';
 import { CompanyComponent } from "./company/company.component";
+import { RolesGuard } from "../../@core/guards/roles.guard";
 
 const routes: Routes = [{
   path: '',
   component: ManagementComponent,
+  canActivate: [RolesGuard],
+  data: { roles: ['ROLE_ADMIN'] },
   children: [
     {
       path: 'company',
