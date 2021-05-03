dd4ea00f2faacaabcfdd0056d98594cb1c7c6880
rcebrian <rodrigocebriang@gmail.com>
2021-05-03 02:52:23 +0200

    remove dead code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,7 +12,6 @@ import { ThemeModule } from './@theme/theme.module';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import {
-  NbChatModule,
   NbDatepickerModule,
   NbDialogModule,
   NbMenuModule,
@@ -34,9 +33,6 @@ import {
     NbDialogModule.forRoot(),
     NbWindowModule.forRoot(),
     NbToastrModule.forRoot(),
-    NbChatModule.forRoot({
-      messageGoogleMapKey: 'AIzaSyA_wNuCzia92MAmdLRzmqitRGvCF7wCZPY',
-    }),
     CoreModule.forRoot(),
     ThemeModule.forRoot(),
   ],
