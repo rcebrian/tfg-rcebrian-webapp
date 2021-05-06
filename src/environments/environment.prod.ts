adaf66c454bed2582acde1314dc1ba25bc9236d2
rcebrian <rodrigocebriang@gmail.com>
2021-05-06 20:26:42 +0200

    :wrench: environment configuration

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -8,16 +8,22 @@ const usersBase = `http://localhost:3001/api/users`;
 const companyBase = `http://localhost:3002/api/company`;
 
 export const environment = {
-  production: true,
+  production: false,
 
   authBase: authBase,
   usersBase: usersBase,
 
   auth: {
     signIn: `/signIn`,
+    signOut: `/signOut`,
+    signUp: `${authBase}/signUp`,
+    getRoles: `${authBase}/roles`,
   },
   users: {
-    getUserInfo: `${usersBase}/:id`
+    getAllUsers: `${usersBase}`,
+    getUserInfo: `${usersBase}/:id`,
+    putUserInfo: `${usersBase}/:userId`,
+    deleteUserInfo: `${usersBase}/:userId`,
   },
   companies: {
     getTreeAllCompanies: `${companyBase}/tree`,
