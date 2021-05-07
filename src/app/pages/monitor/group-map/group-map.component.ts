b26226678e485f6437e39f1ea74509d0be75ea75
rcebrian <rodrigocebriang@gmail.com>
2021-05-07 10:31:40 +0200

    group map component

---
+++ src/app/pages/monitor/group-map/group-map.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ngx-group-map',
+  templateUrl: './group-map.component.html',
+  styleUrls: ['./group-map.component.scss']
+})
+export class GroupMapComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
