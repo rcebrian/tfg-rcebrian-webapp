140becdc691b66eab07f820c8155a342dd22cf84
rcebrian <rodrigocebriang@gmail.com>
2021-05-11 20:12:43 +0200

    inicializacion del mapa en componente

--- src/app/pages/monitor/group-map/map/map.component.ts
+++ src/app/pages/monitor/group-map/map/map.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { MapService } from './map.service';
+import * as mapboxgl from 'mapbox-gl';
 
 @Component({
   selector: 'ngx-map',
@@ -8,11 +9,45 @@ import { MapService } from './map.service';
 })
 export class MapComponent implements OnInit {
 
-  constructor(private map: MapService) {
+  map: mapboxgl.Map | undefined;
+  style = `mapbox://styles/mapbox/outdoors-v11`;
+  // Coordenadas de la localización donde queremos centrar el mapa
+  lat = 40.4293283;
+  lng = -3.7046224;
+  zoom = 15;
+
+  constructor(private mapService: MapService) {
+  }
+
+  private initializeMap() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(position => {
+        this.lat = position.coords.latitude;
+        this.lng = position.coords.longitude;
+        this.map.flyTo({
+          center: [this.lng, this.lat],
+        });
+      });
+    }
+
+    this.buildMap();
+  }
+
+  buildMap() {
+    this.map = new mapboxgl.Map({
+      container: 'map',
+      style: this.style,
+      zoom: 13,
+      center: [this.lng, this.lat],
+    });
+
+    this.map.addControl(new mapboxgl.NavigationControl());
+
+
   }
 
   ngOnInit(): void {
-    this.map.buildMap();
+    this.initializeMap();
   }
 
 }
--- src/app/pages/monitor/group-map/map/map.service.ts
+++ src/app/pages/monitor/group-map/map/map.service.ts
@@ -8,23 +8,9 @@ import * as mapboxgl from 'mapbox-gl';
 })
 export class MapService {
   mapbox = (mapboxgl as typeof mapboxgl);
-  map: mapboxgl.Map | undefined;
-  style = `mapbox://styles/mapbox/outdoors-v11`;
-  // Coordenadas de la localización donde queremos centrar el mapa
-  lat = 40.4293283;
-  lng = -3.7046224;
-  zoom = 15;
+
   constructor() {
     // Asignamos el token desde las variables de entorno
     this.mapbox.accessToken = environment.mapbox.accessToken;
-  }
-  buildMap(): void {
-    this.map = new mapboxgl.Map({
-      container: 'map',
-      style: this.style,
-      zoom: this.zoom,
-      center: [this.lng, this.lat],
-    });
-    this.map.addControl(new mapboxgl.NavigationControl());
   }
 }
